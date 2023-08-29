ef7e755f572525763dc80e7f90f0000c5eaabc8e
Артемий <artema121rus@mail.ru>
2023-08-29 17:16:17 +0300

    Добавляет проверку ПП блока Обучение-таблет

--- source/js/modules/sliders/init-slider-instructors.js
+++ source/js/modules/sliders/init-slider-instructors.js
@@ -17,11 +17,11 @@ const initSliderInstructors = () => {
       breakpoints: {
         1200: {
           slidesPerView: 4,
-          spaceBetween: 30,
         },
 
         768: {
           slidesPerView: 3,
+          spaceBetween: 30,
         },
 
         320: {
