5b2ab01ef9576959e18e7a84e4548353d7cdfa84
Артемий <artema121rus@mail.ru>
2023-09-01 13:09:27 +0300

    Добавляет доработку проекта - аудио в блоке Hero

--- source/js/modules/intro/init-audio-player.js
+++ source/js/modules/intro/init-audio-player.js
@@ -5,11 +5,8 @@ const wrapper = document.querySelector('.intro-audio__wrapper');
 const createAudio = () => {
   const audio = document.createElement('iframe');
 
-  audio.setAttribute('src', 'https://music.yandex.ru/album/25474374/track/112912322?activeTab=track-list');
-  audio.setAttribute('title', 'YouTube video player');
+  audio.setAttribute('src', 'https://music.yandex.ru/iframe/#track/112912322/25474374');
   audio.setAttribute('frameborder', '0');
-  audio.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
-  audio.setAttribute('allowfullscreen', '');
   audio.classList.add('description__audio-player');
   container.append(audio);
 };
