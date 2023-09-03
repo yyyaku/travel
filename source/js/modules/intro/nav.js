bef83f4726502eaf5716779c5f4f8d46d05f38c5
Артемий <artema121rus@mail.ru>
2023-09-03 19:18:39 +0300

    Добавляет правильную функцию на кнопку навигации

--- source/js/modules/intro/nav.js
+++ source/js/modules/intro/nav.js
@@ -1,4 +1,5 @@
 const introHeader = document.querySelector('.intro-header');
+const introLogo = document.querySelector('.intro-header__logo');
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
@@ -6,15 +7,10 @@ const navButtonClick = () => {
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-      introHeader.classList.add('intro-header--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-      introHeader.classList.remove('intro-header--opened');
-    }
+    navMain.classList.toggle('main-nav--closed');
+    navMain.classList.toggle('main-nav--opened');
+    introHeader.classList.toggle('intro-header--opened');
+    introLogo.classList.toggle('logo--dark');
   });
 };
 
