dc1032ceac26ecafc88d54a4a325f7169f7a410c
Артемий <artema121rus@mail.ru>
2023-09-05 08:49:02 +0300

    Добавляет фиксы меню

--- source/js/modules/intro/nav.js
+++ source/js/modules/intro/nav.js
@@ -2,6 +2,8 @@ const introHeader = document.querySelector('.intro-header');
 const introLogo = document.querySelector('.intro-header__logo');
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 
 const navButtonClick = () => {
   navMain.classList.remove('main-nav--nojs');
@@ -11,6 +13,17 @@ const navButtonClick = () => {
     navMain.classList.toggle('main-nav--opened');
     introHeader.classList.toggle('intro-header--opened');
     introLogo.classList.toggle('logo--dark');
+    overlay.classList.toggle('overlay--opened');
+    body.classList.toggle('menu--opened');
+  });
+
+  overlay.addEventListener('click', function () {
+    navMain.classList.toggle('main-nav--closed');
+    navMain.classList.toggle('main-nav--opened');
+    introHeader.classList.toggle('intro-header--opened');
+    introLogo.classList.toggle('logo--dark');
+    overlay.classList.toggle('overlay--opened');
+    body.classList.toggle('menu--opened');
   });
 };
 
